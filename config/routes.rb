c464d6559ce5d31ba6b58917de883a66de4db587
Keith Gan <keithgan96@gmail.com>
2018-11-21 14:16:19 +0800

    Recreated user database

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'welcome/index'
+  root to: "welcome#index"
 
   devise_for :users, controllers: {
     sessions: 'users/sessions',
@@ -8,5 +10,5 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   
-  root to: "welcome#index"
+  resources :users
 end
