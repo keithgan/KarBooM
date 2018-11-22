be1d8d08f9ab8909cc3f4c59c440afd835550d1f
Keith Gan <keithgan96@gmail.com>
2018-11-22 11:40:48 +0800

    Updated user feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,28 @@ class UsersController < ApplicationController
     def show
         @user = current_user
     end
+
+    def edit
+        @user = User.find_by(params[:user])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to @user 
+    end
+
+    def user_params
+        params.require(:user).permit(
+          :full_name, 
+          :gender,
+          :ic_number,
+          :driving_license,
+          :number_plate,
+          :email, 
+          :password,
+          :password_confirmation,
+          :current_password
+          )
+      end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :complaints
+         has_many :complaints
+
+  validates :ic_number, format: { with: /\A[\d]{6}-[\d]{2}-[\d]{4}\z/, message: "can only be integers. No letters/signs allowed." }
+  validates :driving_license, format: { with: /\A[\d]{8}\z/, message: "can only be integers. No letters/signs allowed." }
+  validates :number_plate, format: { with: /\A[A-Z]{3}\s[\d]{4}\z/, message: "a in invalid format. "}
 end
