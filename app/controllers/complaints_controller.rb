37b3952404769e7b64e98e606de683d6221a14c1
melissasaw <melissasaw95@hotmail.com>
2018-11-22 11:16:39 +0800

    Added dropdown list for offences and linked to fine amount(added fine column as well)

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -12,10 +12,39 @@ class ComplaintsController < ApplicationController
 
 	def show
 		@complaint = Complaint.find_by(id:params[:format])
+
 	end
 
 	def create
+	
 		complaint= current_user.complaints.new(get_params)
+
+		if (get_params[:offence] == "1") 
+		    offence = "Double Parking with no passenger inside the car"
+		    fine = 100
+		 elsif (get_params[:offence] == "2") 
+		    offence = "Not parking entirely inside the box"
+		    fine = 30
+		 elsif (get_params[:offence] == "3") 
+		    offence = "Parking in OKU without valid disabled permit"
+		    fine = 200
+		 elsif (get_params[:offence] == "4") 
+		    offence = "Parking at a bus station or a bus stand"
+		    fine = 100
+		 elsif (get_params[:offence] == "5") 
+		    offence = "Parking on a yellow/white line"
+		    fine = 80
+		 elsif (get_params[:offence] == "6") 
+		    offence = "Parking somewhere which obstructs traffic"
+		    fine = 250
+		 else
+		     offence = "nil"
+		     fine = nil
+		 end
+
+		complaint.update(offence:offence)
+		complaint.update(fine:fine)
+		byebug
 		if complaint.save
 			redirect_to root_path
 		else
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_172700) do
+ActiveRecord::Schema.define(version: 2018_11_22_111000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_11_21_172700) do
     t.string "number_plate"
     t.string "avatar"
     t.integer "user_id"
+    t.integer "fine"
   end
 
   create_table "users", force: :cascade do |t|
