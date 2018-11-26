e2c1a47e6e0fd68897235d465710b9c19aa7e935
Melissa Saw <melissasaw95@hotmail.com>
2018-11-26 17:51:38 +0800

    Able to look for numberplate from uploaded images (given its distinct)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,20 +7,26 @@ require 'base64'
   def numplate
   	
   	file_upload=get_params[:avatar].tempfile
+  	# change image to base64 filet type with format  "data:image/jpeg;base64,#{@image}"
   	shit =  Base64.strict_encode64(file_upload.read)
-
-  	byebug
-  	file = 'data:image/jpeg'+"\;"+'base64,'+ Base64.strict_encode64(file_upload.read)
-
-
-   
-	# change image to base64 filet type
-	# file = Base64.strict_encode64()
-
+  	file = 'data:image/jpeg'+"\;"+'base64,'+shit
+  
+	# call the api 
 	data = OcrSpace::FilePost.post('/parse/image', body: { apikey: ENV['OCR_KEY'], language: 'eng', isOverlayRequired: true, base64Image: file})    
 	# Hunshen's code: This filters out the stuff we want
+
 	parsed_text = data.parsed_response['ParsedResults'][0]["ParsedText"].gsub(/\r|\n/, "")
-	# call the api 
+	byebug
+
+	if parsed_text.length == nil || parsed_text.length == 0
+
+		#column is empty 
+	else
+		# display the number plate
+	end
+
+
+
   end
 
   private
