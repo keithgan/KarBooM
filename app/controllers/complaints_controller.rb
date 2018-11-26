29c045b7962f90fc2b2716b9689a7f9a05e76305
Melissa Saw <melissasaw95@hotmail.com>
2018-11-26 17:08:27 +0800

    Implemented OCR Space API to get unumberplate from image, added OCR gem and implemented stuff in welcome html and controller

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -18,6 +18,7 @@ class ComplaintsController < ApplicationController
 
 	def create
 		complaint= current_user.complaints.new(get_params)
+		byebug
 
 
 		# NUMBER PLATE RECOGNIZER
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,30 +1,32 @@
 class WelcomeController < ApplicationController
-
+require 'base64'
 
   def index
   end
 
   def numplate
   	
-  
-	url = URI.parse('https://platerecognizer.com/plate-reader/')
-	path =  complaint.avatar.current_path
-	
-	File.open(path) do |jpg|
-	
-	  req = Net::HTTP::Post::Multipart.new url.path,
-	    "upload" => UploadIO.new(jpg, "image/jpeg", path)
-	  req['Authorization'] = '640a016c82e33e4f0268b1e7d55ba9664717b68d '
-	  res = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
-	    http.request(req)
-	  end
-	end
+  	file_upload=get_params[:avatar].tempfile
+  	shit =  Base64.strict_encode64(file_upload.read)
+
+  	byebug
+  	file = 'data:image/jpeg'+"\;"+'base64,'+ Base64.strict_encode64(file_upload.read)
+
+
+   
+	# change image to base64 filet type
+	# file = Base64.strict_encode64()
+
+	data = OcrSpace::FilePost.post('/parse/image', body: { apikey: ENV['OCR_KEY'], language: 'eng', isOverlayRequired: true, base64Image: file})    
+	# Hunshen's code: This filters out the stuff we want
+	parsed_text = data.parsed_response['ParsedResults'][0]["ParsedText"].gsub(/\r|\n/, "")
+	# call the api 
   end
 
   private
 
   def get_params
-  	params.permit(avatar:{})
+  	params.permit(:avatar)
   end
 
 
